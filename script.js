a1fbfb4bd46a2f12962aad605ebd15b1cc64dc2f
ali829 <alielyassraoui@gmail.com>
2022-12-14 10:44:49 +0100

    formation category and slick slider added

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
-var swiper = new Swiper(".mySwiper", {
-    grabCursor: true,
+$(document).ready(function() {
+  // slick caroussel
+  $('.courses-category-slider').slick({
+    centerMode: true,
+    centerPadding: '50px',
+    slidesToShow: 2,
+    autoplaySpeed: 4000,
+    dots: true,
+    customPaging : function(slider, i) {
+        return '<span class="slider-dot"></span>';
+    },
+    rtl:  true,
+    // autoplay: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: false,
+          slidesToShow: 1
+        }
+      }
+    ]
   });
+});
