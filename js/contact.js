5ce429fefe5e283726976762401a964d85c1c5cb
ali829 <alielyassraoui@gmail.com>
2022-12-20 18:26:07 +0100

    modify contact js

--- js/contact.js
+++ js/contact.js
@@ -14,16 +14,16 @@ document.querySelector('.elmas-contact-form').addEventListener('submit',function
     const message = document.getElementById('contact-subject');
     const phoneNumber = document.getElementById('contact-phone');
     const fullName = document.getElementById('contact-name');
-    Email.send({
-        Host : "smtp.gmail.com",
-        Username : "alielyassraoui@gmail.com",
-        Password : "ixkcdtklnjclrbcj",
-        To : 'alielyassraoui@gmail.com',
-        From : email ,
-        Subject : `message: ${message} , phone: ${phoneNumber} , full name: ${fullName}`,
-        Body : "And this is the body"
-    }).then(
-      message => alert(message)
-    );
+    // Email.send({
+    //     Host : "smtp.gmail.com",
+    //     Username : "alielyassraoui@gmail.com",
+    //     Password : "ixkcdtklnjclrbcj",
+    //     To : 'alielyassraoui@gmail.com',
+    //     From : email ,
+    //     Subject : `message: ${message} , phone: ${phoneNumber} , full name: ${fullName}`,
+    //     Body : "And this is the body"
+    // }).then(
+    //   message => alert(message)
+    // );
     
 })
